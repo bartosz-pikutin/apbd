b94181419c9c517e0a8bf8d53193158dfa80daba
bartosz-pikutin <s24819@pjwstk.edu.pl>
2024-05-13 03:40:01 +0200

    drobna poprawa

---
+++ Zad4/Zad4/Model/Zapytanie.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zad4.Model;
+
+public class Zapytanie
+{
+    [Required]
+    public int IdProduct { get; set; }
+    [Required]
+    public int IdWarehouse { get; set; }
+    [Required]
+    public int Amount { get; set; }
+    [Required]
+    [DataType(DataType.DateTime)]
+    public DateTime CreatedAt { get; set; }
+
+    public bool Read()
+    {
+        throw new NotImplementedException();
+    }
+}
